c6088bce9acf669b36981bea1cda281ba26aed86
Thales de Oliveira da Costa <105451422+ThalesOCosta@users.noreply.github.com>
2023-04-05 16:44:38 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,28 @@
+"""
+1 -
+
+
+class Pessoa:
+
+    def __init__(self, nome, idade, altura):
+        self.nome = nome
+        self.idade = idade
+        self.altura = altura
+
+    def mostra_dados(self):
+        print(f'Nome: {self.nome}')
+        print(f'Idade: {self.idade}')
+        print(f'Altura: {self.altura}')
+
+
+pessoa = Pessoa(input('Nome: '), input('Idade: '), input('Altura: '))
+pessoa.mostra_dados()
+
+
+"""
+"""
+2 - 
+
+"""
+
+
